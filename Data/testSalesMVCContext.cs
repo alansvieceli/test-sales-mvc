889313118dba95c84abefb8e500c5c742fed9997
Alan Vieceli <alansvieceli@gmail.com>
2019-12-04 21:53:17 -0300

    Refazendo as classe com Scaffolding

---
+++ Data/testSalesMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace testSalesMVC.Models
+{
+    public class testSalesMVCContext : DbContext
+    {
+        public testSalesMVCContext (DbContextOptions<testSalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<testSalesMVC.Models.Department> Department { get; set; }
+    }
+}
