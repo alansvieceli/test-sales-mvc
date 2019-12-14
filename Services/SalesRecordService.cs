d61532cc3f9900eccc511cfbf41aab146bc67ec2
Alan Vieceli <alansvieceli@gmail.com>
2019-12-13 22:37:24 -0300

    Vendas

---
+++ Services/SalesRecordService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testSalesMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace testSalesMVC.Services {
+
+    public class SalesRecordService {
+
+        private readonly testSalesMVCContext _context;
+
+        public SalesRecordService(testSalesMVCContext context) {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate) {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue) {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue) {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate) {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue) {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue) {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+    }
+}
